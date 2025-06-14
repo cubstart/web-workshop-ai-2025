58ecb36ac4fb089a68888b3a6b4b2ff43b9fe0cd
meicib <ameicib@berkeley.edu>
2025-06-14 15:05:34 -0700

    Adjust anchor tags

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,8 +3,8 @@ import React from 'react';
 const About = () => {
   return (
     <>
-        <h2>About Me</h2>
-        <section id="about" className="About">
+        <h2 id="about">About Me</h2>
+        <section className="About">
             <p>Hi 👋 I'm Mei, a rising senior at UC Berkeley studying Computer Science!</p>
             <p>My interests include frontend development, computer security, and many more of the interdisciplinary applications of CS. Check out my projects below!</p>
         </section>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Contact = () => {
   return (
     <>
-        <h2>Contact</h2>
-        <section id="contact" className="Contact">
+        <h2 id="contact">Contact</h2>
+        <section className="Contact">
             <p>Email: <a href="mailto:mei@hackberkeley.org">mei@hackberkeley.org</a></p>
         </section>
     </>
