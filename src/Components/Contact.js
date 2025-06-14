5e253958f17b5c460ff5061ca5be6fc85b535a5e
meicib <ameicib@berkeley.edu>
2025-06-14 15:29:18 -0700

    Add linkedin and github icons

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -6,6 +6,10 @@ const Contact = () => {
         <h2 id="contact">Contact</h2>
         <section className="Contact">
             <p>Email: <a href="mailto:mei@hackberkeley.org">mei@hackberkeley.org</a></p>
+            <div className="Contact-icons">
+                <a href="https://www.linkedin.com/in/amelie-cibulka" target="_blank" rel="noopener noreferrer"><FaLinkedin className="icon"/></a>
+                <a href="https://github.com/meicib" target="_blank" rel="noopener noreferrer"><FaGithubSquare className="icon"/></a>
+            </div>
         </section>
     </>
   );
